c857b2ece4de76845205aa4b2ee681667ce36c9f
Aldo89 <31159437+Aldo89@users.noreply.github.com>
2023-06-24 16:13:59 +0200

    Coerced the data types and filled the column bundesland partly.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # e.on
+
+Issues in the dataset:
+
+1. The column order_date is a charcter column so I converted it to datetime.
+2. In the column postcode the values are not just 5 digits.
+   1. The strings appear to have float values like 80687.0
+   2. There is a value in the column that contains non-digit values
+   3. There are postcodes that only have 4 digits and not 5
+
+3. In the column bundesland there are 29532 missing values. I fill it with 
  
--- main.py
+++ main.py
@@ -2,6 +2,38 @@
 import pandas as pd
 
 # Read data
-data = pd.read_csv('data/interview_signup.csv.gz', nrows=100, compression='gzip')
-print(data.head(10))
+data = pd.read_csv('data/interview_signup.csv.gz', compression='gzip')
+
+# Date is a charecter and we coescre it to date.
+data['order_date'] = pd.to_datetime(data['order_date'])
+
+# In the head I see that the postcode have .0 values. I will delete them
+data['postcode'] = data['postcode'].astype('str')
+data['postcode'] = data['postcode'].str.replace(r'.0','')
+
+# Column postcode can not be coerced into int because it contains values other than digits. So I delete the entries with
+# non-digit values.
+data['postcode_is_digit'] = list(map(lambda x: x.isdigit(), data['postcode']))
+cnt_false = data['postcode_is_digit'].value_counts()
+
+data.loc[data['postcode_is_digit'] == False, 'postcode'] = None
+
+# Remove column postcode_is_digit
+data = data.drop(columns='postcode_is_digit')
+
+# Find missing data
+
+
+# Drop rows where postcode and bundesland are missing
+data = data.drop(data[(data['postcode'].isnull()) & (data['bundesland'].isnull())].index)
+
+# Fill postcodes with the legth of 4 with a 0 in front to get legth 5.
+data['postcode'] = data.postcode.astype(str).str.pad(5,fillchar='0')
+
+# Fill missing values
+data = data.sort_values(['postcode', 'bundesland'], ascending=[True, False])
+data['key'] = data['postcode']
+data = data.groupby('key').fillna(method="ffill")
+
+
 
