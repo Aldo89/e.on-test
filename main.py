95aa2797c4769a7a110b143162595989332e54d9
Aldo89 <31159437+Aldo89@users.noreply.github.com>
2023-06-24 18:30:51 +0200

    Worked on missing values in bundesland column

--- main.py
+++ main.py
@@ -21,19 +21,23 @@ data.loc[data['postcode_is_digit'] == False, 'postcode'] = None
 # Remove column postcode_is_digit
 data = data.drop(columns='postcode_is_digit')
 
-# Find missing data
-
-
 # Drop rows where postcode and bundesland are missing
 data = data.drop(data[(data['postcode'].isnull()) & (data['bundesland'].isnull())].index)
 
 # Fill postcodes with the legth of 4 with a 0 in front to get legth 5.
 data['postcode'] = data.postcode.astype(str).str.pad(5,fillchar='0')
 
-# Fill missing values
+# Find multiple combination of postcode and bundesland
+post_bund = data.loc[:, ['postcode', 'bundesland']]
+post_bund_unq = post_bund.drop_duplicates()
+post_bund_unq['cnt'] = post_bund_unq.groupby(['postcode'])['postcode'].transform('count')
+
+# Drop column combinations between postcode and bundesland that occure more than 2 times.
+data = data.merge(post_bund_unq)
+data = data[data.cnt < 3]
+data = data.drop(columns='cnt')
+
+# Fill miss values in bundesland
 data = data.sort_values(['postcode', 'bundesland'], ascending=[True, False])
 data['key'] = data['postcode']
 data = data.groupby('key').fillna(method="ffill")
-
-
-
